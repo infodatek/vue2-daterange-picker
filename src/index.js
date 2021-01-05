aee84353efff0b6dc4859260c935a90b84f9d8b1
dhulsman <dhulsman@infodatek.com>
2021-01-05 09:10:00 +0100

    Add install script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import DateRangePicker from './components/DateRangePicker.vue'
+import DateRangePicker from "./components/DateRangePicker.vue";
 
-export default DateRangePicker
+export default {
+  install(Vue, options) {
+    // Let's register our component globally
+    // https://vuejs.org/v2/guide/components-registration.html
+    Vue.component("vue2-daterange-picker", DateRangePicker);
+  }
+};
